46aecb82ea635a3fc1721a333e0aaa8524d08811
Arnau March <arnau@arnaumarch.com>
2015-01-27 08:54:01 +0100

    allow keyboard navigation

--- slides/js/script.js
+++ slides/js/script.js
@@ -17,17 +17,34 @@ for (var i = 0; i < slides.length; i++) {
 
 function nextSlide(i) {
   document.querySelectorAll('.slide')[i+1].classList.add('show');
+  document.body.dataset.slide = i + 2;
 }
 
 function prevSlide(i) {
   document.querySelectorAll('.slide')[i].classList.remove('show');
+  document.body.dataset.slide = i;
 }
 
 // Avoid loosing focus on tab in contenteditable box
+// Move slides with keyboard
 document.onkeydown = function(e) {
-//document.querySelectorAll('.editable').onkeydown=function(e){
-  console.log(e.keyCode);
-  if(e.keyCode === 9){
+  //console.log(e.keyCode);
+  //console.log(e.target.nodeName);
+  if (e.keyCode === 9){
     e.preventDefault();
   }
+  if (e.target.nodeName != 'STYLE') {
+    if (e.keyCode === 39) {
+      e.preventDefault();
+      nextSlide(currentSlide());
+    }
+    if(e.keyCode === 37){
+      e.preventDefault();
+      prevSlide(currentSlide());
+    }
+  }
+}
+
+function currentSlide() {
+  return document.body.dataset.slide - 1;
 }
